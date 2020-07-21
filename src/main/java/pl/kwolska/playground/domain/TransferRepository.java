0a003affdd5ba4f9c9a7cc628ba38f107693040c
kwolska <Katarzyna.Wolska@empik.com>
2020-07-21 10:13:16 +0200

    Delete TransferRepository

--- src/main/java/pl/kwolska/playground/domain/TransferRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.kwolska.playground.domain;
-
-import pl.kwolska.playground.domain.model.Transfer;
-
-// todo 3: to wyleci!
-public interface TransferRepository {
-  
-  void addTransfer(Transfer transfer);
-}
